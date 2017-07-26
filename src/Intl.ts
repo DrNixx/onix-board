30f58f5015c95905baacb5bdb632e6f30f063001
Nick Panteleeff <drnix@chess-online.com>
2017-07-26 17:36:25 +0300

    Update Intl

--- src/Intl.ts
+++ src/Intl.ts
@@ -1,28 +1,16 @@
 import { Intl as IntlCore } from 'onix-core';
+import { registerStrings as IntlChess } from 'onix-chess';
 
 var intlInitialized = false;
 
 export function registerStrings() {
     if (!intlInitialized) {
         
-        IntlCore.registerStrings('builder', {
+        IntlChess();
+
+        IntlCore.registerStrings('chess', {
             'ru-ru': {
-                fen: "FEN",
                 size: "Размер",
-                pieces: "Фигуры",
-                squares: "Доска",
-                who_move: "Очередь хода",
-                white: "Белые",
-                black: "Черные",
-                white_move: "Ход белых",
-                black_move: "Ход черных",
-                move_no: "№ хода",
-                ep_target: "e.p.",
-                castle: "Рокировка",
-                firstMove: "Первый ход",
-                prevMove: "Предыдущий ход",
-                nextMove: "Следующий ход",
-                lastMove: "Последний ход",
                 timer_lessone: "менее 1",
                 timer_empty: "---",
                 timer_nolabel: ["", "", "", ""],
@@ -33,22 +21,7 @@ export function registerStrings() {
             },
 
             'en-us': {
-                fen: "FEN",
                 size: "Size",
-                pieces: "Pieces",
-                squares: "Board",
-                who_move: "Who move",
-                white: "White",
-                black: "Black",
-                white_move: "White move",
-                black_move: "Black move",
-                move_no: "Move no",
-                ep_target: "e.p.",
-                castle: "Castle",
-                firstMove: "First move",
-                prevMove: " Prev move",
-                nextMove: "Next move",
-                lastMove: "Last move",
                 timer_lessone: "less 1",
                 timer_empty: "---",
                 timer_nolabel: ["", "", "", ""],
