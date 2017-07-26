a51d00ae5b7243c1d5d705dc7297765bc7d3773c
Nick Panteleeff <drnix@chess-online.com>
2017-07-26 13:10:54 +0300

    Add export

--- src/chess/index.ts
+++ src/chess/index.ts
@@ -1,5 +1,13 @@
+import * as BoardActionTypes from './BoardActionTypes';
+import * as BoardActionConsts from './BoardActionConsts';
+import * as BoardActions from './BoardActions';
+
 export { ChessPiece } from './ChessPiece';
 export { ChessSquare } from './ChessSquare';
 export { ChessBoard } from './ChessBoard';
 export { BoardSize, BoardMode, Orientation } from './Constants';
 export { BoardSettings } from './BoardSettings';
+export { boardReducer } from './BoardReducer';
+export { BoardState, BoardRelatedStore } from './BoardState';
+
+export { BoardActionTypes, BoardActionConsts, BoardActions };
