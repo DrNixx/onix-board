89e008b2853e6c0711ff82c6d06a953a03b6c838
Nick Panteleeff <drnix@chess-online.com>
2017-08-02 12:44:25 +0300

    Add export. Update packages

--- src/chess/index.ts
+++ src/chess/index.ts
@@ -8,6 +8,7 @@ export { ChessSquare } from './ChessSquare';
 export { ChessBoard } from './ChessBoard';
 export { ChessDragLayer } from './ChessDragLayer';
 export { BoardSize, BoardSizeClass, BoardMode, Orientation } from './Constants';
+export { BoardSelection, BoardMovement } from './BoardSelection';
 export { BoardSettings } from './BoardSettings';
 export { boardReducer } from './BoardReducer';
 export { BoardState, BoardRelatedStore } from './BoardState';
